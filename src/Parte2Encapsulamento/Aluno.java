a8dfc927e71add948650efdb70a1f2b120018889
lucas <mr.pine97@gmail.com>
2021-08-25 10:29:36 -0300

    override

--- src/Parte2Encapsulamento/Aluno.java
+++ src/Parte2Encapsulamento/Aluno.java
@@ -19,7 +19,7 @@ public class Aluno {
     notaReavaliacao = nReavaliação;
   }
 
-  @Override//reescrever o código
+  @Override//reescrever o código apenas para esse filho
   public String toString() {
     return "Aluno [ Nome: " + nome + ", Matrícula: " + matricula + ", Nota 1: " + nota1 + ", Nota 2: " + nota2 + ", Reavaliação: " + notaReavaliacao + "]";
   }
