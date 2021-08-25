261470c54a070576c97683b958eebdcc51f3c1c9
lucas <mr.pine97@gmail.com>
2021-08-25 10:52:18 -0300

    quebra de linha

--- src/Parte2Encapsulamento/Aluno.java
+++ src/Parte2Encapsulamento/Aluno.java
@@ -21,7 +21,9 @@ public class Aluno {
 
   @Override//reescrever o código apenas para esse filho
   public String toString() {
-    return "Aluno [ Nome: " + nome + ", Matrícula: " + matricula + ", Nota 1: " + nota1 + ", Nota 2: " + nota2 + ", Reavaliação: " + notaReavaliacao + ", Média: " + calcularMedia() + "]";
+    return "Aluno [ Nome: " + nome + ", Matrícula: " + matricula
+    + ", Nota 1: " + nota1 + ", Nota 2: " + nota2 + ", Reavaliação: "
+    + notaReavaliacao + ", Média: " + calcularMedia() + "]";
   }
 
   public double calcularMedia() {
