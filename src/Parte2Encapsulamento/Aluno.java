4eb48bfbbe1466fa8578b539ec4aed7c02d95aa4
lucas <mr.pine97@gmail.com>
2021-08-25 09:48:39 -0300

    definindo metodo construtor

---
+++ src/Parte2Encapsulamento/Aluno.java
@@ -0,0 +1,13 @@
+package Parte2Encapsulamento;
+
+public class Aluno {
+  String nome;
+  String matricula;
+  double nota1;
+  double nota2;
+  double notaReavaliacao;
+
+  Aluno() {
+    System.out.println("passei pelo método construtor");
+  }
+}
--- src/Parte2Encapsulamento/aula02.java
+++ src/Parte2Encapsulamento/aula02.java
@@ -1,5 +1,10 @@
 package Parte2Encapsulamento;
 
 public class aula02 {
-  
+  public static void main(String[] args) {
+    Aluno aluno1 = new Aluno();//método construtor
+    aluno1.nome = "Lucas";
+
+    System.out.println("Aluno: " + aluno1);
+  }
 }
