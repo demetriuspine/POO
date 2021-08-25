f384c4bb1257f92a6825ec6f39dd3c06c5b4fa81
lucas <mr.pine97@gmail.com>
2021-08-25 10:20:37 -0300

    toString original

--- src/Parte2Encapsulamento/aula02.java
+++ src/Parte2Encapsulamento/aula02.java
@@ -4,14 +4,12 @@ public class aula02 {
   public static void main(String[] args) {
     Aluno aluno1 = new Aluno();//método construtor
     aluno1.nome = "bartolomeu";
-    System.out.println("aluno1" + aluno1.nome);
+
+    System.out.println(aluno1.toString());
 
     Aluno aluno2 = new Aluno("pedro", "81622073", 7, 8, 0);
 
-    System.out.println(aluno2.nome);
-    System.out.println(aluno2.matricula);
-    System.out.println(aluno2.nota1);
-    System.out.println(aluno2.nota2);
+    System.out.println(aluno2.toString());
 
   }
 }
